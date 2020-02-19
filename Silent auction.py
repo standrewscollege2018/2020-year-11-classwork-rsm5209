420bf62506a4ffcdaa36febe5b215a34f2fa4bd4
rsm5209 <60585354+rsm5209@users.noreply.github.com>
2020-02-20 11:22:01 +1300

    Update Silent auction.py

--- Silent auction.py
+++ Silent auction.py
@@ -7,10 +7,14 @@ error_catch = True
 Item_Name = input("What is the auction for?\n")
 while error_catch == True:
     try:
-        Reserve_Price = int(input("What is the reserve price?\n"))
-        error_catch = False
+        reserve_price = int(input("What is the reserve price?\n"))
+        if reserve_price < 0:
+            print("Please enter a valid reserve price")
+            error_catch = True
+        else:
+            error_catch = False
     except:
-        print("Please remove the dollar sign")
+        print("Please remove the dollar sign or enter a vaild number")
 
 print("The auction for {} is underway.".format(Item_Name))
 error_catch = True
@@ -19,15 +23,25 @@ while error_catch == True:
         name = input("What is the bidders name?\n")
         try:
             bid = int(input("Enter your bid\n"))
+            if bid == -1:
+                    print("The auction has ended. Well done {} on winning the {} with a bid of {}".format(highest_name, Item_Name, highest_bid))
+                    error_catch = False
+            elif bid > highest_bid:
+                print("The highest bidder is {} with a bid of {} dollars".format(name, bid))
+                highest_bid = bid
+                highest_name = name  
+            else:
+                print("This bid does not exceed the highest bid, please enter a higher amount")
+                
         except:
-            print("Please remove the dollar sign")
-        if bid > highest_bid:
-            print("The highest bidder is {} with a bid of {} dollars".format(name, bid))
-            highest_bid = bid
-        else:
-            print("This bid does not exceed the highest bid, please enter a higher amount")
+            print("Please remove the dollar sign or write a vaild number")
     except:
         print("This is not a valid entry, please try again")
+
+if highest_bid < reserve_price:
+    print("I'm sorry this item did not sell as the highest bid did not exceed the reserve price")
+
+
         
         
         
